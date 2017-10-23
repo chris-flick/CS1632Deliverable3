215cadbf00ec09cb23aba56a09df18a091f245f0
Bin Dong <bin@Bins-MacBook-Pro.local>
2017-10-23 10:44:12 -0400

    added test

--- D3Test.java
+++ D3Test.java
@@ -17,7 +17,7 @@ public class D3Test{
 	static WebDriver driver = new HtmlUnitDriver();
 
 	@BeforeClass
-    public static void setUpDriver() {
+		public static void setUpDriver() {
 
 		// Note that the logging level is a Java standard (thus the
 		// use of a java.util class instead of something specific
@@ -28,16 +28,18 @@ public class D3Test{
 		// (only show the most egregious of errors).
 		// A full lists of levels and descriptions are located at:
 		// https://docs.oracle.com/javase/7/docs/api/java/util/logging/Level.html
-	
-		java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF); 
+
+		java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
 		System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
-		driver = new HtmlUnitDriver(); 
-	
-    }
-	
+		driver = new HtmlUnitDriver();
+
+		}
+
 	// Start at the home page for the deliverable for each test
 	@Before
 	public void setUp() throws Exception {
+		java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
+		System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
 		driver.get("https://cs1632ex.herokuapp.com");
 	}
 
@@ -53,6 +55,21 @@ public class D3Test{
 			assertTrue(elementText.contains("to a land of pure calculation"));
 		}
 		catch(NoSuchElementException nseex){
+			fail();
+		}
+	}
+
+	//Testing to see if there are links
+	@Test
+	public void linkTest(){
+
+		try{
+			driver.findElement(By.linkText("CS1632 D3 Home"));
+			driver.findElement(By.linkText("Factorial"));
+			driver.findElement(By.linkText("Fibonacci"));
+			driver.findElement(By.linkText("Hello"));
+			driver.findElement(By.linkText("Cathedral Pics"));
+		}catch(NoSuchElementException nseex){
 			fail();
 		}
 	}
